fddc33e9738be9c2e2fcaae39eb768494e367125
wangzhonghuan <wangzhonghuan@360.net>
2018-02-08 16:16:19 +0800

    add img process function

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var request = require('request'),
     cheerio = require('cheerio'),
-    fs = require('fs');
+    fs = require('fs'),
+    jimp = require('jimp');
 
 var images = [];
 
-request('https://www.pexels.com/search/cat/', function(err, res, body) {
+request('https://www.pexels.com/search/mountain/', function(err, res, body) {
     if(!err && res.statusCode === 200) {
         var $ = cheerio.load(body);
         $('img.photo-item__img').each(function() {
             var img = $(this).attr('src');
             images.push(img);
         })
-        for (var i = 0; i < images.length; i++) {
-            request(images[i]).pipe(fs.createWriteStream('images/cat-' + i + '.jpg'));
+        for (let i = 0; i < images.length; i++) {
+            let fileName = 'images/mountain-' + i + '.jpg'
+            request(images[i]).pipe(fs.createWriteStream(fileName))
+                .on('close', function() {
+                    jimp.read(fileName, function (err, img) {
+                        if (err) throw err;
+                        img.quality(60)
+                            .greyscale()
+                            .write("images/mountain-grey-" + i + ".jpg");
+                    });
+                })
         }
     }
 })
